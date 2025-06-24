9d147747f366744066b488bee8bab7273ab0786e
XrMob <jeesmon.xr@gmail.com>
2025-06-24 16:14:23 -0400

    Refactor schema.py to add VisualSearchRequest and related response models

--- backend/schema.py
+++ backend/schema.py
@@ -1,8 +1,10 @@
 from sre_constants import SUCCESS
 from urllib import response
 from mediapipe.calculators import video
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from typing import Optional, List
+
+from sklearn.datasets import descr
 
 class UrlAnalyzeRequest(BaseModel):
     youtube_url: str
@@ -13,6 +15,10 @@ class ChatRequest(BaseModel):
 
 class TimestampsRequest(BaseModel):
     video_url: str
+
+class VisualSearchRequest(BaseModel):
+    youtube_url: str
+    search_query: str
 
 class Timestamp(BaseModel):
     time: str
@@ -40,3 +46,22 @@ class VideoAnalysisResponse(BaseModel):
     video_summary: str
     summary_timestamps: List[SummaryTimestamp]
     has_transcripts: bool
+
+class VideoDescription(BaseModel):
+    timestamps: int
+    description: str
+    embedding: list[float]
+
+class VideoEmbeddingResponse(BaseModel):
+    video_id: str
+    descriptions: List[VideoDescription]
+
+class VisualSearchResult(BaseModel):
+    timestamp: int
+    description: str
+    similarity_score: float
+
+class VisualSearchResponse(BaseModel):
+    video_id: str
+    search_query: str
+    results: List[VisualSearchResult]
